8b87ffa3d34fcf12c5f73ac2e01d99c565d949d2
Awa DIAGNE <awa94.diagne@gmail.com>
2023-03-26 17:02:30 +0000

    Sanitize Post Confirmation Lambda SQL query

--- files/lambdas/cruddur-post-confirmation.py
+++ files/lambdas/cruddur-post-confirmation.py
@@ -21,18 +21,24 @@ def lambda_handler(event, context):
           cognito_user_id
           ) 
         VALUES(
-          '{user_display_name}',
-          '{user_email}',
-          '{user_handle}',
-          '{user_cognito_id}'
+          %s,
+          %s,
+          %s,
+          %s
         )
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('PROD_CONNECTION_URL'))
       cur = conn.cursor()
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql, params)
 
-      cur.execute(sql)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
