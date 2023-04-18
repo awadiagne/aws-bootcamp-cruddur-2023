e98f07043f42738fd91f5b35fb5bff96d01e9364
Awa DIAGNE <awa94.diagne@gmail.com>
2023-04-18 13:51:10 +0000

    Fix Expiring Token

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,19 +1,33 @@
 import { Auth } from 'aws-amplify';
 
-const checkAuth = async (setUser) => {
+export async function getAccessToken(){
+  Auth.currentSession()
+  .then((cognito_user_session) => {
+    const access_token = cognito_user_session.accessToken.jwtToken;
+    localStorage.setItem("access_token", access_token);
+  })
+  .catch((err) => console.log(err));
+}
+
+export async function checkAuth(setUser){
   Auth.currentAuthenticatedUser({
     bypassCache: false 
   })
   .then((user) => {
     console.log('user',user);
     return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
+  })
+  .then((cognito_user) => {
+    console.log('cognito_user',cognito_user);
+    setUser({
+      display_name: cognito_user.attributes.name,
+      handle: cognito_user.attributes.preferred_username
+    })
+    return Auth.currentSession()
+  })
+  .then((cognito_user_session) => {
+      console.log('cognito_user_session',cognito_user_session);
+      localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
   })
   .catch((err) => console.log(err));
 };
-
-export default checkAuth;
