9f42a2ac1d0c14029a12c407e6f0b9d20158a561
Awa DIAGNE <awa94.diagne@gmail.com>
2023-07-08 15:48:04 +0000

    Implement Profile

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -57,6 +57,10 @@ export default function ReplyForm(props) {
     setMessage(event.target.value);
   }
 
+  const close = ()=> {
+    console.log('close')
+  }
+  
   let content;
   if (props.activity){
     content = <ActivityContent activity={props.activity} />;
@@ -65,7 +69,7 @@ export default function ReplyForm(props) {
 
   if (props.popped === true) {
     return (
-      <div className="popup_form_wrap">
+      <div className="popup_form_wrap" onClick={close}>
         <div className="popup_form">
           <div className="popup_heading">
           </div>
